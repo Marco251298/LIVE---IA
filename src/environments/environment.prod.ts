3f6359de3275b629980b9c23e5e9b0224459cf41
moises-miguel-16 <moises.miguel@colegiocima.edu.pe>
2021-08-16 17:08:08 +0200

    cambios hoy 16082021

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCYQxhFWqwQF6vtNu31neKR5CrH0uNIdNY",
+    authDomain: "cachimbomaps.firebaseapp.com",
+    projectId: "cachimbomaps",
+    storageBucket: "cachimbomaps.appspot.com",
+    messagingSenderId: "898071271924",
+    appId: "1:898071271924:web:96b17ef9771b7ba42c41a1",
+    measurementId: "G-MR2N30CJBQ"
+      }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCYQxhFWqwQF6vtNu31neKR5CrH0uNIdNY",
+    authDomain: "cachimbomaps.firebaseapp.com",
+    projectId: "cachimbomaps",
+    storageBucket: "cachimbomaps.appspot.com",
+    messagingSenderId: "898071271924",
+    appId: "1:898071271924:web:96b17ef9771b7ba42c41a1",
+    measurementId: "G-MR2N30CJBQ"
+  }
 };
 
 /*
